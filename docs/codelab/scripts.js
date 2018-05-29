4df7513ee0d39fd9e807a4997df4339d2f857df1
yostane <yostane@yahoo.fr>
2018-05-29 18:36:11 +0200

    Ajout raccourci ok sur mac

--- docs/codelab/scripts.js
+++ docs/codelab/scripts.js
@@ -2,15 +2,15 @@ const API_ROOT = "https://reqres.in/api/";
 const api = name => API_ROOT + name;
 
 let nbAttendees = 10,
-    nbMaxAttendees = 30;
+	nbMaxAttendees = 30;
 
-function fetchAttendees(){
+function fetchAttendees() {
 	return fetch(api(`users?per_page=${nbAttendees}`))
 		.then(res => res.json())
 		.then(res => res.data || [])
 }
 
-function renderAttendees(attendees){
+function renderAttendees(attendees) {
 	const section = document.getElementById("attendees");
 	section.innerHTML = `
 	<h1>Attendees: ${nbAttendees} / ${nbMaxAttendees}</h1>
@@ -30,13 +30,15 @@ function renderAttendees(attendees){
 	updateRegisterForm();
 }
 
-function updateRegisterForm(){
+function updateRegisterForm() {
 	const section = document.getElementById("register");
 	const isFull = (nbAttendees >= nbMaxAttendees);
-	section.querySelectorAll("input, button").forEach(elm => { elm.disabled = isFull });
-	section.querySelector(".status").innerHTML = isFull
-		? `Sorry, this event is full.`
-		: `Some places are still available for you to register for this event.`
+	section.querySelectorAll("input, button").forEach(elm => {
+		elm.disabled = isFull
+	});
+	section.querySelector(".status").innerHTML = isFull ?
+		`Sorry, this event is full.` :
+		`Some places are still available for you to register for this event.`
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -45,14 +47,15 @@ document.addEventListener("DOMContentLoaded", () => {
 	//TODO: Etape 2 - Installation du Service Worker au chargement du document
 	if ('serviceWorker' in navigator) {
 		navigator.serviceWorker
-		  .register('sw.js')
-		  .then(function(serviceWorker) {
-			console.log('Service Worker registered: ' + serviceWorker);
-		  })
-		  .catch(function(error) {
-			console.log('Error registering the Service Worker: ' + error);
-		  });
-		}
+			.register('sw.js', {
+				scope: '.' // <--- THIS BIT IS REQUIRED
+			}).then(function (serviceWorker) {
+				console.log('Service Worker registered: ' + serviceWorker);
+			})
+			.catch(function (error) {
+				console.log('Error registering the Service Worker: ' + error);
+			});
+	}
 
 	//TODO: Etape 4 - Réception de messages depuis le Service Worker
 });
