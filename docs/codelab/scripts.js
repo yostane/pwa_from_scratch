b183b6a02c57273d7a21c67631c577b333513fe6
yostane <yostane@yahoo.fr>
2018-06-05 21:46:50 +0200

    ça maaarche

--- docs/codelab/scripts.js
+++ docs/codelab/scripts.js
@@ -1,17 +1,18 @@
 const API_ROOT = "https://reqres.in/api/";
 const api = name => API_ROOT + name;
 
-let nbAttendees = 10,
+let nbAttendees = 5,
 	nbMaxAttendees = 30;
 
 function fetchAttendees() {
 	return fetch(api(`users?per_page=${nbAttendees}`))
 		.then(res => res.json())
-		.then(res => res.data || [])
+		.then(res => res.data || []);
 }
 
 function renderAttendees(attendees) {
 	const section = document.getElementById("attendees");
+	nbAttendees = attendees.length;
 	section.innerHTML = `
 	<h1>Attendees: ${nbAttendees} / ${nbMaxAttendees}</h1>
 	<ul>
@@ -58,4 +59,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
 	}
 
 	//TODO: Etape 4 - Réception de messages depuis le Service Worker
+	navigator.serviceWorker.onmessage = event => {
+		const message = JSON.parse(event.data);
+		if (message.type && message.type.includes("/api/users")) {
+			console.log("Liste des participants à jour", message.data);
+			renderAttendees(message.data.data);
+		}
+	};
+});
+
+let installPromptEvent;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+	// Prevent Chrome <= 67 from automatically showing the prompt
+	event.preventDefault();
+	// Stash the event so it can be triggered later.
+	installPromptEvent = event;
+	// Update the install UI to notify the user app can be installed
+	const btinInstall = document.querySelector('#install-button');
+	btinInstall.disabled = false;
+
+	btnInstall.addEventListener('click', () => {
+		// Update the install UI to remove the install button
+		btinInstall.disabled = true;
+		// Show the modal add to home screen dialog
+		installPromptEvent.prompt();
+		// Wait for the user to respond to the prompt
+		installPromptEvent.userChoice.then((choice) => {
+			if (choice.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+			// Clear the saved prompt since it can't be used again
+			installPromptEvent = null;
+		});
+	});
 });
--- docs/codelab/sw.js
+++ docs/codelab/sw.js
@@ -1,28 +1,78 @@
-var CACHE_NAME = 'V1';
-var STATIC_FILES = ['/', '/scripts.js'];
-self.addEventListener('install', function (event) {
+var CACHE_NAME = 'V4';
+var STATIC_FILES = ['/', '/scripts.js', '/styles.css'];
+self.addEventListener('install', event => {
     console.log('Service Worker installing.');
-    self.caches.open(CACHE_NAME).then(function (cache) {
-        cache.addAll(STATIC_FILES);
-    }).catch(function (error) {
-        console.log('caching error ' + error);
-    });
+    const promise = self.caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES));
+    event.waitUntil(promise);
 });
 
-self.addEventListener('fetch', function (event) {
+self.addEventListener('activate', event => {
+    const promise = caches.keys().then(keys => {
+        keys.filter(key => key !== CACHE_NAME).forEach(key => caches.delete(key));
+    });
+    event.waitUntil(promise);
+});
+
+self.addEventListener('fetch', (event) => {
     if (!event.request.url.startsWith('http')) {
         return;
     }
-    //Personnalisation de la réponse
-    event.respondWith(
-        //ON vérifie si la requête a déjà été mise en cache
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        }).catch(function (error) {
-            console.log('fetch or match error ' + error);
-        })
-    );
+
+    const key = event.request.url.includes("/api/users") ? 'toto' : event.request;
+
+    event.respondWith(getPrecachePromise(event.request, key));
+
+    if (!event.request.url.includes("/api/users")) {
+        return;
+    }
+
+    const updateRefreshPromise = caches.match(key)
+        .then(cachedResponse => update(event.request))
+        .then(response =>
+            //on retroune la réponse à la fin pour pouvoir chainer avec un refresh car le put retourne une promesse de void
+            caches.open(CACHE_NAME)
+            .then(cache => cache.put(key, response.clone()))
+            .then(_ => response)
+        ).then(response => refresh(response));
+    event.waitUntil(updateRefreshPromise);
 });
+
+const wait = (delay) => new Promise((resolve, reject) => setTimeout(resolve, delay));
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function update(request) {
+    const url = request.url.replace(/per_page=\d+/, `per_page=${getRandomInt(1, 15)}`);
+    return wait(1000)
+        .then(_ => fetch(url))
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network error');
+            }
+            return response;
+        });
+}
+
+function refresh(response) {
+    const generateMessage = (newData) => {
+        const json = {
+            type: response.url,
+            data: newData
+        };
+        return JSON.stringify(json);
+    };
+
+    return response.json().then(newData => {
+        self.clients.matchAll().then(clients => {
+            clients.forEach(client => client.postMessage(generateMessage(newData)));
+        });
+    });
+}
+
+
+
+const getPrecachePromise = (request, key) => caches.match(key).then(cachedResponse => cachedResponse ||  fetch(request));
